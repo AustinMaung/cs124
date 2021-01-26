d50b4d11a5a9eedfa9fc53829a4c313c06080b27
Austin Maung <tecknicoal@gmail.com>
2021-01-25 16:57:41 -0800

    second change to test2

--- test2/test2.cpp
+++ test2/test2.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 
 int main() {
-    std::cout << "Hello World!, this is a second file test";
+    std::cout << "Hello World!, this is a second file test" << std::endl;
+    std::cout << "im gonna try to commit and push this change";
     return 0; 
 }
